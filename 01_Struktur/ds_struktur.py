490fe0686ff11f6f23412698e0a64a9951de7789
Andreas Ulrich <andy.ulrich@erasand.ch>
2021-10-31 10:02:29 +0100

    Create ds_struktur.py

---
+++ 01_Struktur/ds_struktur.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'''
+dx_struktur.py - [d]atei[s]struktur
+Copyright (c) Oktober 2021: Andreas Ulrich
+<http://erasand.ch>, <andreas@erasand.ch>
+
+LIZENZ
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DEUTSCHE ÜBERSETZUNG: <http://www.gnu.de/documents/gpl-3.0.de.html>
+'''
+
+
+if __name__ == '__main__':
+    # Programm
